069cf529e9375fee0517ed1e6845b21b139195be
corok <57379016+corok@users.noreply.github.com>
2026-01-20 16:22:59 -0600

    Bugs Fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,36 @@
 #include <Arduino.h>
 #include <BLEGamepadClient.h>
+#include <ESP32Servo.h>
 // put function declarations here:
 int myFunction(int, int);
 XboxController controller;
+Servo Steering;
+int steeringPin = 5; // Example pin for steering servo
+int motorSpeedPin = 18; // Example pin for motor speed (PWM)
+int motorDirPin = 19;   // Example pin for motor direction
+int motorDirPin2 = 21;  // Example pin for motor direction
+int LED = 16;
 void setup() {
   controller.begin();
   Serial.begin(115200);
+  pinMode(motorSpeedPin, OUTPUT);
+  pinMode(motorDirPin, OUTPUT);
+  pinMode(motorDirPin2, OUTPUT);
+  Steering.attach(steeringPin); // Example pin for steering servo
 }
 
 void loop() {
-  if(controller.isConnected()) {
-    XboxControlsState state;
-    controller.read(&state);
-    Serial.print("Left Stick X: ");
-    Serial.print(state.leftStickX);
-    Serial.print(" Left Stick Y: ");
-    Serial.print(state.leftStickY);
-    Serial.print(" Right Stick X: ");
-    Serial.print(state.rightStickX);
-    Serial.print(" Right Stick Y: ");
-    Serial.print(state.rightStickY);
-    Serial.print(" Left Trigger: ");
-    Serial.print(state.leftTrigger);
-    Serial.print(" Right Trigger: ");
-    Serial.println(state.rightTrigger);
+  delay(1000);
+  if (controller.isConnected()) {
+    XboxControlsState s;
+    controller.read(&s);
+
+    Serial.printf("lx: %.2f, ly: %.2f, rx: %.2f, ry: %.2f\n",
+      s.leftStickX, s.leftStickY, s.rightStickX, s.rightStickY);
   } else {
-    Serial.println("Controller not connected");
+    Serial.println("controller not connected");
   }
 }
-
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
