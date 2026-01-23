e6fdaea761d39038a996e1f3a850e8aedbd7afcb
corok <maboganes@gmail.com>
2026-01-23 12:59:05 -0600

    Throttle+Steering logic added

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,13 @@
 // put function declarations here:
 int myFunction(int, int);
 XboxController controller;
-Servo Steering;
+Servo steerServo;
 int steeringPin = 5; // Example pin for steering servo
 int motorSpeedPin = 18; // Example pin for motor speed (PWM)
 int motorDirPin = 19;   // Example pin for motor direction
 int motorDirPin2 = 21;  // Example pin for motor direction
+float steering =0.0;
+float throttle = 0.0;
 int LED = 16;
 void setup() {
   controller.begin();
@@ -16,17 +18,31 @@ void setup() {
   pinMode(motorSpeedPin, OUTPUT);
   pinMode(motorDirPin, OUTPUT);
   pinMode(motorDirPin2, OUTPUT);
-  Steering.attach(steeringPin); // Example pin for steering servo
+  steerServo.attach(steeringPin); // Example pin for steering servo
 }
 
 void loop() {
-  delay(1000);
+  delay(10);
   if (controller.isConnected()) {
     XboxControlsState s;
     controller.read(&s);
 
-    Serial.printf("lx: %.2f, ly: %.2f, rx: %.2f, ry: %.2f\n",
-      s.leftStickX, s.leftStickY, s.rightStickX, s.rightStickY);
+    steering = s.leftStickX;
+    
+    throttle = s.rightTrigger-s.leftTrigger;
+
+    if(throttle>0){
+      digitalWrite(motorDirPin, HIGH);
+      digitalWrite(motorDirPin2, LOW);
+    }else{
+      digitalWrite(motorDirPin, HIGH);
+      digitalWrite(motorDirPin2, LOW);
+    }
+    analogWrite(motorSpeedPin, abs(throttle)*254);
+    steerServo.write(steering*45);
+
+    Serial.printf("Steering: %.2f, Throttle: %.2f\n",
+      steering, throttle);
   } else {
     Serial.println("controller not connected");
   }
